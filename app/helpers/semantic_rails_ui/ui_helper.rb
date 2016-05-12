35940968c8c3642392b2b1da01142ad89a04c4df
Igor Jancev <igor@masterybits.com>
2016-05-12 11:37:13 +0200

    add ui_flash_messages helper

--- app/helpers/semantic_rails_ui/ui_helper.rb
+++ app/helpers/semantic_rails_ui/ui_helper.rb
@@ -6,8 +6,27 @@ module SemanticRailsUi
     # </h1>
     def ui_icon_header(title, icon_name)
       content_tag :h1, class: "ui header" do
-        content_tag(:i, '', class: "#{icon_name} icon") + content_tag(:div, title, class: "content")
+        ui_icon(icon_name) + content_tag(:div, title, class: "content")
       end
+    end
+
+    def ui_flash_messages
+      content_tag :div, class: "messages" do
+        messages = ""
+        flash.each do |name, msg|
+          messages += content_tag :div, class: "ui #{name} message" do
+            ui_icon("close") + content_tag(:div, class: "ui header") do
+              icon_name = name == 'error' || name == 'warning' ? :warning : :info
+              ui_icon("#{icon_name} circle") + content_tag(:div, msg, class: "content")
+            end
+          end
+        end
+        messages.html_safe
+      end
+    end
+
+    def ui_icon(icon_name)
+      content_tag(:i, '', class: "#{icon_name} icon")
     end
   end
 end
