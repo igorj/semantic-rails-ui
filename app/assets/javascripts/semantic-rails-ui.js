818cde62cd11f3fdbb440a8c1f4002e3b13cab8f
Igor Jancev <igor@masterybits.com>
2016-05-19 18:42:26 +0200

    Include jquery, jquery-ujs, and turbolinks in semantic-rails-ui.js

--- app/assets/javascripts/semantic-rails-ui.js
+++ app/assets/javascripts/semantic-rails-ui.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require semantic-ui
 //= require semantic-rails-ui/menu
 //= require semantic-rails-ui/message
--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,8 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require semantic-rails-ui
 //= require_tree .
