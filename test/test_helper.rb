7cce0fd6d1de7bc278d59100a9a0f3e4d546e9b9
Igor Jancev <igor@masterybits.com>
2016-05-30 15:16:34 +0200

    add webtest support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
 require 'coveralls'
 Coveralls.wear!
 
-
-require 'minitest/autorun'
-
+# use minitest-reporters to create xmls for jenkins and nicer terminal reporting
 require 'minitest/reporters'
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-# Filter out Minitest backtrace while allowing backtrace from other libraries to be shown.
+require 'minitest/reporters/screenshot_reporter'
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new,
+                          Minitest::Reporters::JUnitReporter.new,
+                          Minitest::Reporters::ScreenshotReporter.new]
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
@@ -21,4 +21,57 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.fixtures :all
+end
+
+# default test class for unit tests
+class UnitTest < ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+end
+
+# configure webtests with capybara and poltergeist (headless)
+require 'capybara/rails'
+require 'capybara_minitest_spec'
+require 'capybara/poltergeist'
+
+CAPYBARA_WINDOW_SIZE = [1024, 768]
+
+if ENV['HEADLESS'] == 'true'
+  # headless driver configuration
+  Capybara.register_driver :poltergeist do |app|
+    poltergeist_options = { debug: false, timeout: 30, js_errors: true, window_size: CAPYBARA_WINDOW_SIZE }
+    Capybara::Poltergeist::Driver.new(app, poltergeist_options)
+  end
+  Capybara.default_driver = :poltergeist
+else
+  # chrome browser driver configuration
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+  Capybara.default_driver = :selenium
+  Capybara.current_session.driver.browser.manage.window.resize_to CAPYBARA_WINDOW_SIZE[0], CAPYBARA_WINDOW_SIZE[1]
+end
+Capybara.default_max_wait_time = 5   # how long should capybara wait for elements to appear on the page (ajax)
+
+# use database_cleaner to clean the database after every test with truncation, instead of using transactional fixtures
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+
+# default test class for webtests
+class WebTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  extend Minitest::Spec::DSL
+  include Minitest::Reporters::Screenshot
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # disable transactional fixtures, as we use DatabaseCleaner
+  self.use_transactional_fixtures = false
+
+  # clean database after each test with database_cleaner
+  def after_teardown
+    super
+    DatabaseCleaner.clean
+  end
 end
